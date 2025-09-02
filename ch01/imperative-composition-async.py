bed992b9d896303515372cf4413da5be6092579f
2hnk <kimgh921@gmail.com>
2025-09-02 23:45:23 +0900

    modify file name

--- ch01/imperative-composition-async.py
+++ ch01/imperative-composition-async.py
@@ -1,6 +1,8 @@
-from config import get_model
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import chain
+
+import asyncio
+from config import get_model
 
 
 # 구성요소
@@ -17,11 +19,11 @@ model = get_model()
 # 함수로 결합한다.
 # 데코레이터 @chain을 추가해 작성한 함수에 Runnable 인터페이스를 추가한다.
 @chain
-def chatbot(values):
-    prompt = template.invoke(values)
-    for token in model.stream(prompt):
-        yield token
+async def chatbot(values):
+    prompt = await template.ainvoke(values)
+    return await model.ainvoke(prompt)
 
 
-for part in chatbot.stream({"question": "LLM은 어디서 제공하나요?"}):
-    print(part)
+async def main():
+    result = await chatbot.ainvoke({"question": "LLM은 어디서 제공하나요?"})
+    print(result.content)
--- ch01/imperative-composition-stream.py
+++ ch01/imperative-composition-stream.py
@@ -1,8 +1,6 @@
+from config import get_model
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import chain
-
-import asyncio
-from config import get_model
 
 
 # 구성요소
@@ -19,11 +17,11 @@ model = get_model()
 # 함수로 결합한다.
 # 데코레이터 @chain을 추가해 작성한 함수에 Runnable 인터페이스를 추가한다.
 @chain
-async def chatbot(values):
-    prompt = await template.ainvoke(values)
-    return await model.ainvoke(prompt)
+def chatbot(values):
+    prompt = template.invoke(values)
+    for token in model.stream(prompt):
+        yield token
 
 
-async def main():
-    result = await chatbot.ainvoke({"question": "LLM은 어디서 제공하나요?"})
-    print(result.content)
+for part in chatbot.stream({"question": "LLM은 어디서 제공하나요?"}):
+    print(part)
